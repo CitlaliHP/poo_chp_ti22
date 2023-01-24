de509e857cdccc735b998cdb841bcad8400fc7d2
CitlaliHP <122800835+CitlaliHP@users.noreply.github.com>
2023-01-24 14:57:46 +0000

    Aprender a utilizar .format con texto

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 |1|programa0.py|Hola mundo en Phyton3|
 |2|programa1.py|Comentarios multilinea, comentarios de una linea, concatenacion de una o mas cadenas de texto|
 |3|programa2.py|Aprender a utilizar '.format' y varias formas de lograr un error en el codigo|
+|4|programa3.py|Aprender a utilizar .format con texto|
---
+++ semana2/programa3.py
@@ -0,0 +1,15 @@
+"""
+    Programa3
+    Nombre: Citlali Hernandez Perez
+    Fecha: 23/01/2023
+    Descripción:
+    Operaciones matematicas dentro del 'print' y '.format'
+"""
+variable1 = 10
+variable2 =  5
+print(variable1 + variable2) #lo suma
+print("{}+{}=".format (variable1, variable2),(variable1 + variable2)) #mi resultado pero mezclando dos temas
+print("{}+{}={}".format (variable1, variable2,(variable1 + variable2))) #suma
+print("{}={}*{}".format ((variable1 * variable2),variable1, variable2)) #multiplicación
+print("{}-{}={}".format (variable1, variable2,(variable1 - variable2))) #resta
+print("el resultado de dividir{}/{} es {}".format (variable1, variable2,(variable1 / variable2)))# divicion con string 
