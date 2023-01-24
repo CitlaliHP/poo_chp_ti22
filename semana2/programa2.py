bcc039d03539a8b63f181d5ed1056eb6d43716cd
CitlaliHP <122800835+CitlaliHP@users.noreply.github.com>
2023-01-24 14:06:16 +0000

    Aprender a utilizar '.format' y varias formas de lograr un error en el codigo

---
+++ semana2/programa2.py
@@ -0,0 +1,17 @@
+"""
+    Programa2
+    Nombre: Citlali Hernandez Perez
+    Fecha: 24/01/2023
+    Descripción:
+    Print invocando variables utilizando llaves
+"""
+variable1 = 'hola'
+variable2 =  619
+#print(variable1 + variable2) #concatenar
+print("{}{}".format(variable1, variable2)) #concatenar con llaves
+#formas de descomponerlo 
+#print("{}{}".format(variable1 + variable2))
+#print("{}{}",format(variable1, variable2))
+#print("{}{}".format variable1, variable2)
+#print({}{}.format(variable1, variable2))
+#print("{}{}".format(variable1. variable2))
